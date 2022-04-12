0848638797f8b854b009e9b05a0f5ee4ef64d0f2
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-04-12 16:42:41 +0200

    removed requirements form setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import setuptools 
+import setuptools
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 with open("requirements_WORKING.txt", encoding="utf-8") as f:
-    requirements = f.read().split("\n") 
+    requirements = f.read().split("\n")
 
 setuptools.setup(
     name="relationextraction",
@@ -17,18 +17,14 @@ setuptools.setup(
     url="https://github.com/HLasse/Multi2OIE",
     license="MIT",
     packages=setuptools.find_packages(),
-    install_requires=requirements,
-
-
-    keywords = ['NLP', 'knowledge graphs', 'relation extraction', 'triplets'],
-    
-    classifiers = [
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Windoiws 10',
-        'Environment :: GPU :: NVIDIA CUDA :: 10.0',   
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-
+    install_requires=[],
+    keywords=["NLP", "knowledge graphs", "relation extraction", "triplets"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Windoiws 10",
+        "Environment :: GPU :: NVIDIA CUDA :: 10.0",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
     ],
 )
